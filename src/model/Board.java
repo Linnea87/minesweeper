84b8b0116c3ef78e1bb9a200c4a29ebbbac7beaa
Linnéa <linnea.ternevik@gmail.com>
2025-10-23 11:08:15 +0200

    added skeleton code to Board.java

--- src/model/Board.java
+++ src/model/Board.java
@@ -1,4 +1,29 @@
 package model;
 
 public class Board {
+    public void initialize() {
+    }
+
+    public void createBoard() {
+    }
+
+    public void calculateAdjacentMines {
+
+    }
+
+    public void revealCell(Cell cell) {
+    }
+
+    public void isGameOver() {
+    }
+
+    public void toggleFlag {
+
+    }
+
+    public void isGameWon() {
+    }
+
+
+
 }
