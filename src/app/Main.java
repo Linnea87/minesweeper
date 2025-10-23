f239b36f5a07079bfaab40affd409180eca1dec0
Linnéa <linnea.ternevik@gmail.com>
2025-10-23 15:26:13 +0200

    Board.initialize(): create grid and link in Game.start()

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,6 +4,8 @@ package app;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Game game = new Game();
+        game.start();
 
     }
 }
--- src/model/Cell.java
+++ src/model/Cell.java
@@ -1,4 +1,24 @@
 package model;
 
 public class Cell {
+
+    // === Fields ==============================================================
+
+    private boolean hasMine;
+
+    // === Constructor =========================================================
+
+    public Cell() {
+        this.hasMine = false;
+    }
+
+    // === Core methods ========================================================
+
+    public boolean hasMine() {
+        return hasMine;
+    }
+    public void setMine(boolean hasMine) {
+        this.hasMine = hasMine;
+    }
+
 }
